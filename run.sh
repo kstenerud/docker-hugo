9b0541fccdf0f51fe0f33844944164546e7e8094
Karl Stenerud <kstenerud@gmail.com>
2019-03-31 11:38:00 +0200

    Don't pass in baseurl or theme since those can be configured in the blog. Force port to 80, and don't append port number. Use latest hugo.

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ SLEEP="${HUGO_REFRESH_TIME:=-1}"
 HUGO_DESTINATION="${HUGO_DESTINATION:=/output}"
 echo "HUGO_WATCH:" $WATCH
 echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
-echo "HUGO_THEME:" $HUGO_THEME
-echo "HUGO_BASEURL" $HUGO_BASEURL
 echo "ARGS" $@
 
 HUGO=/usr/local/sbin/hugo
@@ -16,10 +14,10 @@ while [ true ]
 do
     if [[ $HUGO_WATCH != 'false' ]]; then
 	    echo "Watching..."
-        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="$HUGO_DESTINATION" --baseURL="$HUGO_BASEURL" --bind="0.0.0.0" "$@" || exit 1
+        $HUGO server --watch=true --source="/src" --destination="$HUGO_DESTINATION" --bind="0.0.0.0" --port=80 --appendPort=false "$@" || exit 1
     else
 	    echo "Building one time..."
-        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="$HUGO_DESTINATION" --baseURL="$HUGO_BASEURL" "$@" || exit 1
+        $HUGO --source="/src" --destination="$HUGO_DESTINATION" "$@" || exit 1
     fi
 
     if [[ $HUGO_REFRESH_TIME == -1 ]]; then
